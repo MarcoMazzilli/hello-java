3271c4322c7e279c00cf97855539744d5f00778d
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-09-12 15:50:18 +0200

    First Commit

---
+++ src/org/lesson/java/StampaSaluto.java
@@ -0,0 +1,19 @@
+package org.lesson.java;
+
+public class StampaSaluto {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java");
+		
+		String name = "Giovanni";
+		
+		
+		if(name == "giovanni") {
+			System.out.println("Hello Giovanni");
+		}else {
+			System.out.println("Controlla la maiuscola `~`");
+		}
+
+	}
+}
